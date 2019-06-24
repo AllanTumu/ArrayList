8012ffc813b960c58ed2a819eff2df6a192818cb
AllanTumu <allantumu1962@gmail.com>
2019-06-24 14:29:35 +0300

    Initial commit

---
+++ src/String.kt
@@ -0,0 +1,10 @@
+fun main (args:Array<String>){
+    var sentence:String?
+    sentence = "Welcome on board"
+
+    println(sentence)
+    println("small letters: " +sentence.toLowerCase())
+    println("Upper case letters: " +sentence.toUpperCase())
+    println("Array pick: " +sentence[5])
+    println("Split: " +sentence.split(""))
+}
